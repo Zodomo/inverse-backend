b7c42bd86674ddc83de2334ea1482a243ac8e718
Zodomo <zodomo@proton.me>
2024-08-29 23:08:17 -0500

    added extra logic in prep for testnet deployment

--- script/Inverse.s.sol
+++ script/Inverse.s.sol
@@ -5,5 +5,20 @@ import {Script, console2} from "forge-std/Script.sol";
 import {Inverse} from "../src/Inverse.sol";
 
 contract InverseScript is Script {
+    Inverse inverse;
+    uint256 deployerPrivateKey;
+    address deployer;
 
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        deployer = vm.addr(deployerPrivateKey);
+        vm.createSelectFork("baseMainnet");
+    }
+
+    function deploy() public {
+        vm.startBroadcast(deployerPrivateKey);
+        inverse = new Inverse(deployer, address(0), 0.001 ether);
+        vm.stopBroadcast();
+        console2.log("Deployment Address:", address(inverse));
+    }
 }
--- src/Inverse.sol
+++ src/Inverse.sol
@@ -7,10 +7,14 @@ import {SignatureCheckerLib as SCL} from "../lib/solady/src/utils/SignatureCheck
 
 contract Inverse is ERC721, OwnableRoles {
     error AlreadySet();
+    error TransferFailed();
+    error ExcessiveInput();
     error ArrayLengthMismatch();
 
     event Signer(address indexed signer);
     event TokenURI(uint256 indexed tokenId, string indexed metadata);
+    event GasStipend(address indexed addr, uint256 indexed amount);
+    event GasStipendSet(uint256 indexed amount);
 
     string private constant _NAME = "Inverse";
     string private constant _SYMBOL = "INVRS";
@@ -18,18 +22,23 @@ contract Inverse is ERC721, OwnableRoles {
 
     mapping(uint256 tokenId => string) private _metadata;
 
+    mapping(address addr => bool) public hasMinted;
     uint256 public totalSupply;
+    uint256 public gasStipend;
     address public signer;
 
+    // Verifies signature is from frontend
     modifier checkSignature(bytes32 hash, bytes calldata signature) {
         if (!SCL.isValidSignatureNowCalldata(signer, hash, signature)) revert Unauthorized();
         _;
     }
 
-    constructor(address newOwner, address newSigner) payable {
+    constructor(address newOwner, address newSigner, uint256 newGasStipend) payable {
         _initializeOwner(newOwner);
         signer = newSigner;
+        gasStipend = newGasStipend;
         emit Signer(newSigner);
+        emit GasStipendSet(newGasStipend);
     }
 
     function name() public pure override returns (string memory) {
@@ -45,10 +54,35 @@ contract Inverse is ERC721, OwnableRoles {
         return _metadata[tokenId];
     }
 
-    function mint(string memory metadata, bytes32 hash, bytes calldata signature) external checkSignature(hash, signature) {
+    // Pays gas stipend to each minter once
+    function _payStipend(address to) private {
+        uint256 stipend = gasStipend;
+        if (!hasMinted[to] && address(this).balance >= stipend) {
+            hasMinted[to] = true;
+            (bool success,) = payable(to).call{value: stipend}("");
+            if (!success) revert TransferFailed();
+            emit GasStipend(to, stipend);
+        }
+    }
+
+    // Mint logic
+    function _mint(address to, string memory metadata) private {
         uint256 tokenId = ++totalSupply;
-        _mint(msg.sender, tokenId);
+        _metadata[tokenId] = metadata;
+        _mint(to, tokenId);
         emit TokenURI(tokenId, metadata);
+    }
+
+    // Mint via frontend for user with gas
+    function mint(string memory metadata, bytes32 hash, bytes calldata signature) external checkSignature(hash, signature) {
+        _payStipend(msg.sender);
+        _mint(msg.sender, metadata);
+    }
+
+    // Sponsored mint by frontend
+    function mint(address to, string memory metadata) external onlyRolesOrOwner(_OPERATOR) {
+        _payStipend(to);
+        _mint(to, metadata);
     }
 
     function updateTokenURI(uint256[] calldata tokenIds, string[] calldata metadata) external onlyRolesOrOwner(_OPERATOR) {
@@ -59,9 +93,28 @@ contract Inverse is ERC721, OwnableRoles {
         }
     }
 
+    function withdraw(address to, uint256 amount) external onlyRolesOrOwner(_OPERATOR) {
+        if (amount > address(this).balance) revert TransferFailed();
+        (bool success,) = payable(to).call{value: amount}("");
+        if (!success) revert TransferFailed();
+    }
+
+    function assignOperator(address addr, bool status) external onlyOwner {
+        if (status) _grantRoles(addr, _OPERATOR);
+        else _removeRoles(addr, _OPERATOR);
+    }
+
     function changeSigner(address newSigner) external onlyOwner {
         if (signer == newSigner) revert AlreadySet();
         signer = newSigner;
         emit Signer(newSigner);
     }
+
+    function changeStipend(uint256 newGasStipend) external onlyOwner {
+        if (newGasStipend > 1 ether) revert ExcessiveInput();
+        gasStipend = newGasStipend;
+        emit GasStipendSet(newGasStipend);
+    }
+
+    receive() external payable {}
 }
