fe169f9d0eeef9353e033aa4e18e3b1fd878236c
Zodomo <zodomo@proton.me>
2024-09-19 23:42:23 -0500

    update script

--- script/Inverse.s.sol
+++ script/Inverse.s.sol
@@ -5,7 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {Inverse} from "../src/Inverse.sol";
 
 contract InverseScript is Script {
-    Inverse inverse = Inverse(payable(0x100acD34938796aaad6A81528459492d35E4113E)); // Sepolia: 0x100acD34938796aaad6A81528459492d35E4113E // Aleph Zero Testnet: 0x2E28b5Cc90D8A9e4D661417568eb83bdd2DF26C3
+    Inverse inverse = Inverse(payable(0x5A06811Fb0EcF10B98eF4243d5b16fF9b0efbf0C)); // Sepolia: 0x5a06811fb0ecf10b98ef4243d5b16ff9b0efbf0c // Aleph Zero Testnet: 0x2E28b5Cc90D8A9e4D661417568eb83bdd2DF26C3
     uint256 deployerPrivateKey;
     address deployer;
     address signer = 0xfAC83e3eb856e9349bd999843dFB67b9719fA43A;
